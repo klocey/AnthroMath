03a445211ae88371ba4b355348b8fc70ce260bda
klocey <ken@weecology.org>
2015-06-28 15:44:19 -0400

    edited code to handle decimal roots; generated new figs -KL

--- Roots/code/Roots.py
+++ Roots/code/Roots.py
@@ -10,7 +10,7 @@ sys.path.append(mydir + "/GitHub/PeasantMath/Roots/code")
 import functions as fxn
 
 
-ks = [2,3,4,5,6,7,8,9,10]
+ks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5]
 
 for i, k in enumerate(ks):
 
@@ -31,14 +31,20 @@ for i, k in enumerate(ks):
         WLs.append(a)
         x += 1
 
-    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(int(k)))
+    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(k))
     plt.scatter(xs, WLs, color='c', alpha=0.9, label='WHL rule')
     #plt.yscale('log')
     #plt.xscale('log')
     plt.xlabel('x', fontsize=8)
     plt.ylabel('y', fontsize=8)
-    leg = plt.legend(loc=4,prop={'size':12})
-    leg.draw_frame(False)
+    plt.xlim(min(xs), max(xs))
+    plt.ylim(min(WLs), max(rts))
+
+    plt.legend(bbox_to_anchor=(-0.04, 1.08, 2.59, .2), loc=10, ncol=2,
+               mode="expand",prop={'size':16})
+
+    #leg = plt.legend(loc=4,prop={'size':12})
+    #leg.draw_frame(False)
     #plt.text(-50, 14, "How well does the WHL Rule approximate square roots?", fontsize=16)
 
 
@@ -58,15 +64,14 @@ for i, k in enumerate(ks):
         WLs.append(a)
         x += 10
 
-    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(int(k)))
+    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(k))
     plt.scatter(xs, WLs, color='c', alpha=0.9, label='WHL rule')
     plt.yscale('log')
     plt.xscale('log')
     plt.xlabel('x', fontsize=8)
     plt.ylabel('y', fontsize=8)
-    leg = plt.legend(loc=2,prop={'size':12})
-    leg.draw_frame(False)
-
+    plt.xlim(min(xs)*0.5, max(xs)*1.5)
+    plt.ylim(min(WLs)*0.5, max(rts)*1.5)
 
 
     ax = fig.add_subplot(2,2,3)
@@ -85,15 +90,14 @@ for i, k in enumerate(ks):
         x = x*1.5
 
 
-    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(int(k)))
+    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(k))
     plt.scatter(xs, WLs, color='c', alpha=0.9, label='WHL rule')
     plt.yscale('log')
     plt.xscale('log')
     plt.xlabel('x', fontsize=8)
     plt.ylabel('y', fontsize=8)
-    leg = plt.legend(loc=4,prop={'size':12})
-    leg.draw_frame(False)
-
+    plt.xlim(min(xs)*0.5, max(xs)*1.5)
+    plt.ylim(min(WLs)*0.5, max(rts)*1.5)
 
 
     ax = fig.add_subplot(2,2,4)
@@ -111,17 +115,18 @@ for i, k in enumerate(ks):
         WLs.append(a)
         x = x*2
 
-    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(int(k)))
+    plt.scatter(xs, rts, s=50, color='m', facecolors='none', label='root'+str(k))
     plt.scatter(xs, WLs, color='c', alpha=0.9, label='WHL rule')
     plt.yscale('log')
     plt.xscale('log')
     plt.xlabel('x', fontsize=8)
     plt.ylabel('y', fontsize=8)
-    leg = plt.legend(loc=4,prop={'size':13})
-    leg.draw_frame(False)
+    plt.xlim(min(xs)*0.5, max(xs)*1.5)
+    plt.ylim(min(WLs)*0.5, max(rts)*1.5)
+
 
     plt.tick_params(axis='both', which='major', labelsize=8)
     plt.subplots_adjust(wspace=0.5, hspace=0.3)
-    plt.savefig(mydir+'/GitHub/PeasantMath/Roots/figs/WHL_root'+str(int(k))+'.png', dpi=600)#, bbox_inches = 'tight')#, pad_inches=0)
+    plt.savefig(mydir+'/GitHub/PeasantMath/Roots/figs/WHL_root'+str(k)+'.png', dpi=600)#, bbox_inches = 'tight')#, pad_inches=0)
     print 'finished root',k
     #plt.show()
